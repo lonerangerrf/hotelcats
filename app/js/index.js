71dbf215b56d79de26bd06b634d6d0bc9c558aa2
Ivan Tarasov <lonerangerrf@gmail.com>
2021-12-02 23:41:58 +0400

    все сделал как в прошлом уроке, все доверстал, но вот с js проблема он вроде подключился сначало, а теперь ни в какую не хочет отражать, то что делал ты тогда

---
+++ app/js/index.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const menu = document.querySelector('.js-menu')
+const menuButtonOpen = document.querySelector('.js-menu-open')
+const menuButtonClose = menu.querySelector('.js-menu-close')
+
+menuButtonOpen.addEventListener('click', function(evt) {
+  evt.preventDefault()
+  menu.classList.add('menu_open')
+})
+
+menuButtonClose.addEventListener('click', function(evt) {
+  evt.preventDefault()
+  menu.classList.remove('menu-open')
+})
--- build/js/index.js
+++ build/js/index.js
@@ -1,2 +1,14 @@
 "use strict";
+
+var menu = document.querySelector('.js-menu');
+var menuButtonOpen = document.querySelector('.js-menu-open');
+var menuButtonClose = menu.querySelector('.js-menu-close');
+menuButtonOpen.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  menu.classList.add('menu_open');
+});
+menuButtonClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  menu.classList.remove('menu-open');
+});
 //# sourceMappingURL=index.js.map
